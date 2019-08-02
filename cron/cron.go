13dfafbcd116fb7eadd42fe715b5f21205e7d707
UtheMan <mik.umanski@gmail.com>
2019-08-01 18:59:11 -0700

    #4 makefile and cluster communication with client-go

---
+++ cron/cron.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"os"
+	"path/filepath"
+	//"k8s.io/client-go/plugin/pkg/client/auth"
+	"k8s.io/client-go/tools/clientcmd"
+)
+
+func main() {
+	var kubeconfig *string
+	if home := homeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "genesys-config-k8s-genesys"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	// use the current context in kubeconfig
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	pods, err := clientset.BatchV2alpha1().CronJobs("").List(metav1.ListOptions{})
+	//create test cron job
+	//cronJob := clientset.BatchV1beta1().CronJobs().Create()
+	println(pods)
+}
+
+func homeDir() string {
+	if h := os.Getenv("HOME"); h != "" {
+		return h
+	}
+	return os.Getenv("USERPROFILE") // windows
+}
